f91ece3357c2d1c8f935c9f0f58d904b7a370f16
James Sofra <James.Sofra@gmail.com>
2011-12-26 15:50:05 +1100

    Update documentation of most of the public API operations in core.

--- src/euclij/core.clj
+++ src/euclij/core.clj
@@ -22,35 +22,80 @@
 (defn convert-shapes [& shapes]
   (map *geometry-converter* shapes))
 
-(op/defcsg union "union")
-(op/defcsg difference "difference")
-(op/defcsg sym-difference "sym-difference")
-(op/defcsg intersection "intersection")
+(op/defcsg union
+  "Computes a shape representing the combined points of all the given shapes.")
+(op/defcsg difference
+  "Computes a shape representing the difference of all the shapes by
+  reducing over the shapes, computing at each step a shape containing
+  points making up the first shape that do not make up the second.
+  This operation may be order dependent.")
+(op/defcsg sym-difference
+  "Computes a shape representing the difference of all the shapes by
+  reducing over the shapes, computing at each step a shape containing
+  the points in the first shape not in the second and the points in
+  the second not in the first.")
+(op/defcsg intersection
+  "Computes a shape representing the points shared by all the given shapes.")
 
 (op/defspatialcon boundary "boundary")
-(op/defspatialcon buffer "buffer" distance)
-(op/defspatialcon convex-hull "convex-hull")
-(op/defspatialcon bounding-box "bounding-box")
+(op/defspatialcon buffer
+  "Computes a buffer area around a shape having the width given by distance."
+  distance)
+(op/defspatialcon convex-hull
+  "Computes the smallest convex polygon that contains all the points in the
+  shape.")
+(op/defspatialcon bounding-box
+  "Returns this shapes bounding box as a shape with the points
+  (minx, miny), (maxx, miny), (maxx, maxy), (minx, maxy), (minx, miny).")
 
-(op/defspatialrel intersects? "intersects?")
-(op/defspatialrel disjoint? "disjoint?")
-(op/defspatialrel contains? "contains?")
-(op/defspatialrel covers? "covers?")
-(op/defspatialrel covered-by? "covered-by?")
-(op/defspatialrel crosses? "crosses?")
-(op/defspatialrel overlaps? "overlaps?")
+(op/defspatialrel intersects?
+  "Returns true if this shape intersects the other, false otherwise")
+(op/defspatialrel disjoint?
+  "Returns true if this shape is disjoint to the other, false otherwise
+  Disjoint shapes have no points in common.")
+(op/defspatialrel contains?
+  "Returns true if this shape entirely contains all the points of the other
+  without touching, false otherwises ")
+(op/defspatialrel covers?
+  "Returns true if this shape entirely covers all the points of the other,
+  they may touch, false otherwises " )
+(op/defspatialrel covered-by?
+  "Returns true if the points in this shape are entirely covered by the
+  other shape, false otherwise.")
+(op/defspatialrel crosses?
+  "Returns true if this shape crosses the other shape, false otherwise.")
+(op/defspatialrel overlaps?
+  "Returns true if this shape overlaps the other shape, false otherwise.")
 (op/defspatialrel relate? "relate?")
-(op/defspatialrel within-distance? "within-distance?" distance)
-(op/defspatialrel distance "distance")
+(op/defspatialrel within-distance?
+  "Tests whether the distance from this shape to the other is less than
+  or equal to the given distance." distance)
+(op/defspatialrel distance
+  "Returns the minimum distance between this shape and the other shape.")
 
-(op/defspatialcon centroid "centroid")
-(op/defspatialcon interior-point "interior-point")
-(op/defgenericop envelope "envelope" identity)
+(op/defspatialcon centroid "Returns the geometric centre of the shape.")
+(op/defspatialcon interior-point
+  "Computes an interior point of this shape. An interior point is
+  guaranteed to lie in the interior of the shape, if it possible
+  to calculate such a point exactly. Otherwise, the point may lie
+  on the boundary of the shape.")
+(op/defgenericop envelope
+  "Envelope"
+  identity)
 
-(op/defgenericop area "area" identity)
-(op/defgenericop length "length" identity)
+(op/defgenericop area
+  "Computes the area of a shape.
+  If the shape is non-areal then it returns 0.0"
+  identity)
+(op/defgenericop length
+  "Computes the length of a shape.
+  Linear shapes return their length. Areal shapes return their perimeter."
+  identity)
 
-(op/defgenericop simple? "simple?" identity)
+(op/defgenericop simple?
+  "Returns true if a shape is simple, false otherwise.
+  A shape is simple if the only self-intersections are at boundary points"
+  identity)
 
 (op/defshape point "point")
 (op/defshape line-string "line-string")
